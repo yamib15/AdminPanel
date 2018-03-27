104b17276d7c30eb611f5061dbf78519b5fbe88c
Yamini Bansal <35057441+yamib15@users.noreply.github.com>
2018-03-27 19:31:57 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,64 @@
+﻿var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+    var products = [{
+        name: "Chacha",
+        price: 10,
+        imageurl: "images/images (1).jpg",
+        check: false
+
+    },
+    {
+        name: "Lays",
+        price: 20,
+        imageurl: "images/download.jpg",
+        check:false
+    },
+    {
+        name: "Tropicana",
+        price: 15,
+        imageurl: "images/images (2).jpg",
+        check: false
+
+
+    },
+    {
+        name: "Kurkure",
+        price: 20,
+        imageurl: "images/kurkure.jpg",
+        check: false
+
+    },
+    {
+        name: "Oreo Biscuit",
+        price: 20,
+        imageurl: "images/oreo.jpg",
+        check: false
+
+
+    },
+    {
+        name: "Wafers",
+        price: 15,
+        imageurl: "images/wafers.jpg",
+        check: false
+
+    },
+    {
+        name: "Tiger Biscuit",
+        price: 10,
+        imageurl: "images/tigerbiscuit.jpg",
+        check: false
+
+    }
+    ];
+    $scope.products = products;
+
+    $scope.delete = function () {
+        var newList = $scope.products;
+        $scope.products = [];
+        angular.forEach(newList, function (x) {
+            if (!x.check) $scope.products.push(x);
+        }
+        )
+    }
+});
